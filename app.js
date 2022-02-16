a3d973de7132702a575e84b0f69383159712dec5
kevinypris <99774500+kevinypris@users.noreply.github.com>
2022-02-15 18:57:53 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+function message1(){
+
+    document.getElementById("frase").innerHTML = "Me gustas";
+
+}
+function message2(){
+
+    document.getElementById("frase").innerHTML = "Me gusta que me escribas";
+
+}
+
+function message3(){
+
+    document.getElementById("frase").innerHTML = "Me gusta toda tu persona";
+}
+function message4(){
+
+    document.getElementById("frase").innerHTML = "Me gusta tu piel";
+}
+function message5(){
+
+    document.getElementById("frase").innerHTML = "Me gusta tu pelo despeinado";
+}
+function message6(){
+
+    document.getElementById("frase").innerHTML = "Me gusta que seas tan inteligente";
+}
+function message7(){
+
+    document.getElementById("frase").innerHTML = "Me gusta sepas hablar y escuchar";
+}
+function message8(){
+
+    document.getElementById("frase").innerHTML = "Me gusta que me apoyes en mis cosas";
+}
+function message9(){
+
+    document.getElementById("frase").innerHTML = "Quiero verte en tu mejor version";
+}
+function message10(){
+
+    document.getElementById("frase").innerHTML = "Me gusta que me escribas";
+}
+function message11(){
+
+    document.getElementById("frase").innerHTML = "Me haces un mejor hombre";
+}
+function message12(){
+
+    document.getElementById("frase").innerHTML = "Haces mi vida mas bonita";
+}
+function message13(){
+
+    document.getElementById("frase").innerHTML = "Eres mi persona favorita";
+}
+function message14(){
+
+    document.getElementById("frase").innerHTML = "Me gustan tus besos";
+}
+function message15(){
+
+    document.getElementById("frase").innerHTML = "Eres la mejor";
+}
+function message16(){
+
+    document.getElementById("frase").innerHTML = "Nos vamos a casar?";
+}
+function message17(){
+
+    document.getElementById("frase").innerHTML = "Te extraño";
+}
+function message18(){
+
+    document.getElementById("frase").innerHTML = "Por mas ocupado que este siempre estas en mi cabeza";
+}
+
+
